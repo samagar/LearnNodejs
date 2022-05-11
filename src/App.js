d67bcebafda588c0aafa472adf63429aa8911d36
Sandeep Magar <sandi.magar@gmail.com>
2022-05-11 12:55:53 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h3>This is a Sample Sandeep Magar react App</h3>
+        <h3>This is a Sample Sandeep Manohar Magar react App</h3>
       </div>
     </div>
   );
